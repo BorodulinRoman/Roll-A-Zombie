f361b96563fe3eeb390c2cbdeee4ad448b59c62b
Roman <roma.brodlin@gmail.com>
2021-10-17 12:03:32 +0300

    First common data
    Upload fist git with all basic design .

---
+++ Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	
+	public GameObject selectedZombie;
+	public List<GameObject> zombies;
+	public Vector3 selectedSize;
+	public Vector3 defaultSize;
+
+	// Use this for initialization
+	void Start () {
+		yossi(selectedZombie);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	
+	void yossi(GameObject newZ)
+    {
+		newZ.transform.localScale = selectedSize;
+	}
+
+
+}
